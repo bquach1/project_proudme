7aa4b1a090fed91dc9555efc2124c6e0cf5cdb0b
Bruce Quach <quachbruce@gmail.com>
2022-12-15 14:07:13 -0600

    Add more buttons and start styling

--- project_proudme/src/components/header.js
+++ project_proudme/src/components/header.js
@@ -6,9 +6,18 @@ import '../css/header.css';
 const Header = () => {
     return (
         <nav>
-            <div>
-                <Button style={{backgroundColor: "#B18EEB"}}>ProudMe</Button>
+            <div class="left-nav">
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>PROUDME</Button>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>My Pet</Button>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>My Journal</Button>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>Pet Store</Button>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>ProudME PE</Button>
             </div>
+            {/* <div>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>Sign In</Button>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>ProudME PE</Button>
+                <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>ProudME PE</Button>
+            </div> */}
         </nav>
     );
 }
