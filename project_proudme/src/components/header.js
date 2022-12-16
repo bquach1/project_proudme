c48fad638e41cd12a3527af880d997bbfcc745c4
Bruce Quach <quachbruce@gmail.com>
2022-12-16 00:30:23 -0600

    Finish tentative styles for left nav buttons

--- project_proudme/src/components/header.js
+++ project_proudme/src/components/header.js
@@ -10,33 +10,36 @@ const Header = () => {
 
     return (
         <nav>
-            <div class="left-nav">
+            
+            <div className="left-nav">
                 <Button 
-                    style={{backgroundColor: "#B18EEB", padding: "5px 15px 5px 15px", borderRadius: "50px", color: "white", fontWeight: "bold", fontSize: "20px"}}
+                    style={{backgroundColor: "#B18EEB", padding: "10px 15px 10px 15px", borderRadius: "50px", color: "white", fontWeight: "bold", fontSize: "20px"}}
                     onClick={() => navigate('/home')}
                     >PROUDME</Button>
                 <Button 
-                    style={{backgroundColor: "#FFACE4", borderRadius: "25px", color: "white", fontWeight: "bold", textTransform: "none"}}
+                    style={{backgroundColor: "#FFACE4", padding: "5px 15px 5px 15px", borderRadius: "25px", color: "white", fontWeight: "bold", textTransform: "none", fontSize: "16px"}}
                     onClick={() => navigate('/pet')}
                     >My Pet</Button>
                 <Button 
-                    style={{backgroundColor: "#FF9770", borderRadius: "50px", color: "white", fontWeight: "bold", textTransform: "none"}}
+                    style={{backgroundColor: "#FF9770", padding: "5px 15px 5px 15px", borderRadius: "25px", color: "white", fontWeight: "bold", textTransform: "none", fontSize: "16px"}}
                     onClick={() => navigate('/journal')}
                     >My Journal</Button>
                 <Button 
-                    style={{backgroundColor: "#94E065", borderRadius: "50px", color: "white", fontWeight: "bold", textTransform: "none"}}
+                    style={{backgroundColor: "#94E065", padding: "5px 15px 5px 15px", borderRadius: "25px", color: "white", fontWeight: "bold", textTransform: "none", fontSize: "16px"}}
                     onClick={() => navigate('/pet')}
                     >Pet Store</Button>
                 <Button 
-                    style={{backgroundColor: "#AB87FF", borderRadius: "50px", color: "white", fontWeight: "bold", textTransform: "none"}}
+                    style={{backgroundColor: "#AB87FF", padding: "5px 15px 5px 15px", borderRadius: "25px", color: "white", fontWeight: "bold", textTransform: "none", fontSize: "16px"}}
                     onClick={() => navigate('/pet')}
                     >ProudME PE</Button>
             </div>
-            {/* <div>
+
+            <div className="right-nav">
                 <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>Sign In</Button>
                 <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>ProudME PE</Button>
                 <Button style={{backgroundColor: "#B18EEB", borderRadius: "50px", color: "white", fontFamily: "Roboto"}}>ProudME PE</Button>
-            </div> */}
+            </div>
+
         </nav>
     );
 }
