d5cfd6c52863a5aacb785bbfac30a1133cba9396
Bruce Quach <quachbruce@gmail.com>
2023-07-17 21:52:42 -0700

    Create username validation

--- client/src/screens/signup.js
+++ client/src/screens/signup.js
@@ -9,7 +9,7 @@ import {
   Select,
   MenuItem,
   FormControl,
-  InputLabel
+  InputLabel,
 } from "@mui/material";
 import axios from "axios";
 import { CircularProgress } from "@mui/material";
@@ -21,7 +21,7 @@ const SignUpScreen = () => {
 
   // States for checking the errors
   const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState("");
+  const [loginError, setLoginError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const [form, setForm] = useState({
@@ -70,11 +70,16 @@ const SignUpScreen = () => {
       })
       .catch((error) => {
         console.error(error);
-        if (error.message.data === "Email is already in use") {
-          setError("Email in use");
+        if (error.response.data == "Email is already in use") {
+          console.log('t');
+          setLoginError("Email is already registered. Try signing in or using a different email!");
+        } else if (error.response.data == "Username is already in use!") {
+          setLoginError("Username is already registered. Try signing in or using a different username!")
         }
         setLoading(false);
-        alert("Email is already registered. Try signing in or using a different email!");
+      })
+      .then(() => {
+        alert(loginError);
       });
   };
 
@@ -147,98 +152,94 @@ const SignUpScreen = () => {
         <div className="line-container">
           <div className="row-container">
             <label>Birth Month: </label>
-            <select
+            <Select
               className="dropdown"
               name="month"
               onChange={(e) => updateForm({ birthMonth: e.target.value })}
               required
+              displayEmpty
+              value={form.birthMonth}
+              style={{ width: 190, backgroundColor: "white" }}
             >
-              <option defaultValue="" disabled hidden>
-                Select an option
-              </option>
-              <option value="January">January</option>
-              <option value="February">February</option>
-              <option value="March">March</option>
-              <option value="April">April</option>
-              <option value="May">May</option>
-              <option value="June">June</option>
-              <option value="July">July</option>
-              <option value="August">August</option>
-              <option value="September">September</option>
-              <option value="November">November</option>
-              <option value="December">December</option>
-            </select>
+              <MenuItem disabled value="">
+                <div style={{ opacity: 0.6 }}>Select an Option</div>
+              </MenuItem>
+              <MenuItem value="January">January</MenuItem>
+              <MenuItem value="February">February</MenuItem>
+              <MenuItem value="March">March</MenuItem>
+              <MenuItem value="April">April</MenuItem>
+              <MenuItem value="May">May</MenuItem>
+              <MenuItem value="June">June</MenuItem>
+              <MenuItem value="July">July</MenuItem>
+              <MenuItem value="August">August</MenuItem>
+              <MenuItem value="September">September</MenuItem>
+              <MenuItem value="November">November</MenuItem>
+              <MenuItem value="December">December</MenuItem>
+            </Select>
           </div>
           <div className="row-container">
             <label>Birth Year: </label>
-            <select
+            <Select
               className="dropdown"
               name="year"
               onChange={(e) => updateForm({ birthYear: e.target.value })}
               required
+              displayEmpty
+              value={form.birthYear}
+              style={{ width: 190, backgroundColor: "white" }}
             >
-              <option defaultValue="" disabled hidden>
-                Select an option
-              </option>
-              <option value="1980">1980</option>
-              <option value="1981">1981</option>
-              <option value="1982">1982</option>
-              <option value="1983">1983</option>
-              <option value="1984">1984</option>
-              <option value="1985">1985</option>
-              <option value="1986">1986</option>
-              <option value="1987">1987</option>
-              <option value="1988">1988</option>
-              <option value="1989">1989</option>
-              <option value="1990">1990</option>
-              <option value="1991">1991</option>
-              <option value="1992">1992</option>
-              <option value="1993">1993</option>
-              <option value="1994">1994</option>
-              <option value="1995">1995</option>
-              <option value="1996">1996</option>
-              <option value="1997">1997</option>
-              <option value="1998">1998</option>
-              <option value="1999">1999</option>
-              <option value="2000">2000</option>
-              <option value="2001">2001</option>
-              <option value="2002">2002</option>
-              <option value="2003">2003</option>
-              <option value="2004">2004</option>
-              <option value="2005">2005</option>
-              <option value="2006">2006</option>
-              <option value="2007">2007</option>
-              <option value="2008">2008</option>
-              <option value="2009">2009</option>
-              <option value="2010">2010</option>
-              <option value="2011">2011</option>
-              <option value="2012">2012</option>
-              <option value="2013">2013</option>
-              <option value="2014">2014</option>
-              <option value="2015">2015</option>
-              <option value="2016">2016</option>
-              <option value="2017">2017</option>
-              <option value="2018">2018</option>
-              <option value="2019">2019</option>
-              <option value="2020">2020</option>
-              <option value="2021">2021</option>
-              <option value="2022">2022</option>
-            </select>
+              <MenuItem disabled value="">
+                <div style={{ opacity: 0.6 }}>Select an Option</div>
+              </MenuItem>
+              <MenuItem value="1990">1990</MenuItem>
+              <MenuItem value="1991">1991</MenuItem>
+              <MenuItem value="1992">1992</MenuItem>
+              <MenuItem value="1993">1993</MenuItem>
+              <MenuItem value="1994">1994</MenuItem>
+              <MenuItem value="1995">1995</MenuItem>
+              <MenuItem value="1996">1996</MenuItem>
+              <MenuItem value="1997">1997</MenuItem>
+              <MenuItem value="1998">1998</MenuItem>
+              <MenuItem value="1999">1999</MenuItem>
+              <MenuItem value="2000">2000</MenuItem>
+              <MenuItem value="2001">2001</MenuItem>
+              <MenuItem value="2002">2002</MenuItem>
+              <MenuItem value="2003">2003</MenuItem>
+              <MenuItem value="2004">2004</MenuItem>
+              <MenuItem value="2005">2005</MenuItem>
+              <MenuItem value="2006">2006</MenuItem>
+              <MenuItem value="2007">2007</MenuItem>
+              <MenuItem value="2008">2008</MenuItem>
+              <MenuItem value="2009">2009</MenuItem>
+              <MenuItem value="2010">2010</MenuItem>
+              <MenuItem value="2011">2011</MenuItem>
+              <MenuItem value="2012">2012</MenuItem>
+              <MenuItem value="2013">2013</MenuItem>
+              <MenuItem value="2014">2014</MenuItem>
+              <MenuItem value="2015">2015</MenuItem>
+              <MenuItem value="2016">2016</MenuItem>
+              <MenuItem value="2017">2017</MenuItem>
+              <MenuItem value="2018">2018</MenuItem>
+              <MenuItem value="2019">2019</MenuItem>
+              <MenuItem value="2020">2020</MenuItem>
+              <MenuItem value="2021">2021</MenuItem>
+              <MenuItem value="2022">2022</MenuItem>
+              <MenuItem value="2023">2023</MenuItem>
+            </Select>
           </div>
           <div className="row-container">
             <label>Grade Level: </label>
-            <FormControl>
             <Select
               className="dropdown"
               name="grade"
-              onChange={(e) => updateForm({ gradeLevel: e.target.value })}
-              style={{width: "auto", backgroundColor: "white"}}
+              onChange={(e) => updateForm({ schoolYear: e.target.value })}
+              style={{ width: 190, backgroundColor: "white" }}
               value={form.schoolYear}
               required
               displayEmpty
             >
               <MenuItem disabled value="">
-                <div style={{opacity: 0.6}}>Select an Option</div>
+                <div style={{ opacity: 0.6 }}>Select an Option</div>
               </MenuItem>
               <MenuItem value="prek">Pre-K</MenuItem>
               <MenuItem value="kindergarten">Kindergarten</MenuItem>
@@ -256,29 +257,28 @@ const SignUpScreen = () => {
               <MenuItem value="twelfth">12th</MenuItem>
               <MenuItem value="other">Other</MenuItem>
             </Select>
-            </FormControl>
           </div>
         </div>
         <div className="line-container">
           <div className="input-container">
             <label>Gender: </label>
-            <FormControl>
-              <Select
-                className="dropdown"
-                value={form.gender}
-                name="gender"
-                onChange={(e) => updateForm({ gender: e.target.value })}
-                required
-                displayEmpty
-                style={{width: 200, backgroundColor: "white"}}
-              >
-                <MenuItem disabled value=""><div style={{opacity: 0.6}}>Select an Option</div></MenuItem>
-                <MenuItem value="male">Male</MenuItem>
-                <MenuItem value="female">Female</MenuItem>
-                <MenuItem value="other">Other</MenuItem>
-                <MenuItem value="none">Prefer not to tell</MenuItem>
-              </Select>
-            </FormControl>
+            <Select
+              className="dropdown"
+              value={form.gender}
+              name="gender"
+              onChange={(e) => updateForm({ gender: e.target.value })}
+              required
+              displayEmpty
+              style={{ width: 200, backgroundColor: "white" }}
+            >
+              <MenuItem disabled value="">
+                <div style={{ opacity: 0.6 }}>Select an Option</div>
+              </MenuItem>
+              <MenuItem value="male">Male</MenuItem>
+              <MenuItem value="female">Female</MenuItem>
+              <MenuItem value="other">Other</MenuItem>
+              <MenuItem value="none">Prefer not to tell</MenuItem>
+            </Select>
           </div>
           <div className="input-container">
             <label>Email Address: </label>
@@ -371,9 +371,6 @@ const SignUpScreen = () => {
 };
 
 export default SignUpScreen;
-
-// have left page recommended goals removed after child adds a goal to the right page.
-// automated feedback, move right page to left page after goal added, reflection on right page after click (blank at first).
 
 const styles = {
   messageText: {
