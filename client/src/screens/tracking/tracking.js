ca471490ae07f2f5f6495d39e04df3f81af463cf
Bruce Quach <quachbruce@gmail.com>
2023-07-18 22:33:41 -0500

    Add bar chart option

--- client/src/screens/tracking/tracking.js
+++ client/src/screens/tracking/tracking.js
@@ -7,10 +7,21 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  BarChart,
+  Bar
 } from "recharts";
 import axios from "axios";
 import styled from "styled-components";
-import { Select, Button, MenuItem } from "@mui/material";
+import {
+  Select,
+  Button,
+  MenuItem,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
 
 const FilterWrapper = styled.div`
   display: flex;
@@ -45,6 +56,28 @@ const BehaviorLineChart = ({ data }) => {
   );
 };
 
+const BehaviorBarChart = ({ data }) => {
+  return (
+    <BarChart
+      width={600}
+      height={400}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      title={"Activity Data Graph"}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="formattedDate" label="Time" />
+      <YAxis label="minutes/day" />
+      <Tooltip />
+      <Legend />
+      <Bar
+        dataKey="behaviorValue"
+        fill="#8884d8"
+      />
+    </BarChart>
+  );
+};
+
 // Render the chart component
 const TrackingScreen = () => {
   const [user, setUser] = useState([]);
@@ -52,6 +85,8 @@ const TrackingScreen = () => {
   const [allUsers, setAllUsers] = useState([]);
   const [userInput, setUserInput] = useState("");
   const [loading, setLoading] = useState(true);
+
+  const [chartType, setChartType] = useState("line");
 
   const [activityBehaviorData, setActivityBehaviorData] = useState([]);
   const [screentimeBehaviorData, setScreentimeBehaviorData] = useState([]);
@@ -148,32 +183,64 @@ const TrackingScreen = () => {
   return (
     <>
       <FilterWrapper>
-        <Select
-          placeholder="Search for user"
-          id="input"
-          type="text"
-          label="Search for user's behavior chart"
-          value={loading ? "" : userInput}
-          style={{ width: "200px" }}
-          onChange={(e) => {
-            setUserInput(e.target.value);
-          }}
-        >
-          {allUsers.map((user, index) => (
-            <MenuItem key={index} value={user.name}>
-              {user.name}
-            </MenuItem>
-          ))}
-        </Select>
-        <Button
-          onClick={() => {
-            setShownUser(allUsers.find((user) => user.name === userInput));
-          }}
-          style={{ backgroundColor: "green", color: "white" }}
-        >
-          Submit
-        </Button>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Chart Type</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            value={chartType}
+            name="radio-buttons-group"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <FormControlLabel
+              value="line"
+              control={<Radio onClick={(e) => setChartType(e.target.value)}/>}
+              label="Line Chart"
+            />
+            <FormControlLabel
+              value="bar"
+              control={<Radio onClick={(e) => setChartType(e.target.value)}/>}
+              label="Bar Chart"
+            />
+            <FormControlLabel
+              value="scatter"
+              control={<Radio onClick={(e) => setChartType(e.target.value)}/>}
+              label="Scatterplot"
+            />
+          </RadioGroup>
+        </FormControl>
       </FilterWrapper>
+
+      <Select
+        placeholder="Search for user"
+        id="input"
+        type="text"
+        label="Search"
+        value={loading ? "" : userInput}
+        style={{ width: "200px" }}
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+      >
+        {allUsers.map((user, index) => (
+          <MenuItem key={index} value={user.name}>
+            {user.name}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <Button
+        onClick={() => {
+          setShownUser(allUsers.find((user) => user.name === userInput));
+        }}
+        style={{
+          backgroundColor: "green",
+          color: "white",
+          marginLeft: "1rem",
+          textTransform: "none",
+        }}
+      >
+        Submit
+      </Button>
 
       <div
         style={{
@@ -183,7 +250,13 @@ const TrackingScreen = () => {
         }}
       >
         <h1>{shownUser.name}'s Activity Behavior Data</h1>
+        {chartType === "line" ?
         <BehaviorLineChart data={activityBehaviorData} />
+        : chartType === "bar" ?
+        <BehaviorBarChart data={activityBehaviorData} />
+        : 
+        <BehaviorLineChart data={activityBehaviorData} />
+        }
         <h1>{shownUser.name}'s Screentime Behavior Data</h1>
         <BehaviorLineChart data={screentimeBehaviorData} />
         <h1>{shownUser.name}'s Eating Behavior Data</h1>
