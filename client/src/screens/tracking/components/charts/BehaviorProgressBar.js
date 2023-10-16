b3078f64b665f49835961623cb5d7251197db64b
Bruce Quach <quachbruce@gmail.com>
2023-10-16 12:43:48 -0500

    Concatenate progress bar values and round floats

--- client/src/screens/tracking/components/charts/BehaviorProgressBar.js
+++ client/src/screens/tracking/components/charts/BehaviorProgressBar.js
@@ -2,58 +2,59 @@ import React from "react";
 import { Box, LinearProgress } from "@mui/material";
 
 const BehaviorProgressBar = ({ data, chartGoalType }) => {
+  const totalBehaviorValue = data.reduce(
+    (total, entry) => total + entry.behaviorValue,
+    0
+  );
+  const totalRecommendedValue = data.reduce(
+    (total, entry) => total + entry.recommendedValue,
+    0
+  );
+
+  const overallProgress = (totalBehaviorValue / totalRecommendedValue) * 100;
+
   return (
     <Box sx={{ width: "80%" }} position="relative">
-      {data.map((entry) => {
-        return (
-          <div
-            style={{
-              display: "flex",
-              margin: 10,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div style={{ width: "10%" }}>{entry.date}</div>
-            <LinearProgress
-              variant="determinate"
-              value={
-                (entry.behaviorValue / entry.recommendedValue) * 100 > 100
-                  ? 100
-                  : (entry.behaviorValue / entry.recommendedValue) * 100
-              }
-              style={{ height: 50, borderRadius: 10, flex: 1, margin: 10 }}
-              sx={{
-                "& .MuiLinearProgress-bar": {
-                  backgroundColor:
-                    chartGoalType === "screentime" &&
-                    entry.behaviorValue > entry.recommendedValue * 2
-                      ? "#FF6961"
-                      : chartGoalType === "screentime" &&
-                        entry.behaviorValue === entry.recommendedValue
-                      ? "#8884d8"
-                      : chartGoalType === "screentime" &&
-                        entry.behaviorValue > entry.recommendedValue
-                      ? "#FFC000"
-                      : chartGoalType === "screentime" &&
-                        entry.behaviorValue < entry.recommendedValue
-                      ? "#77DD77"
-                      : entry.behaviorValue > entry.recommendedValue
-                      ? "#77DD77"
-                      : entry.behaviorValue === entry.recommendedValue
-                      ? "#8884d8"
-                      : entry.behaviorValue < entry.recommendedValue / 2
-                      ? "#FF6961"
-                      : "#FFC000",
-                },
-              }}
-            />
-            <div style={{ width: "10%" }}>
-              {entry.behaviorValue} / {entry.recommendedValue}
-            </div>
-          </div>
-        );
-      })}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <LinearProgress
+          variant="determinate"
+          value={overallProgress > 100 ? 100 : overallProgress}
+          style={{ height: 50, borderRadius: 10, flex: 1, margin: 10 }}
+          sx={{
+            "& .MuiLinearProgress-bar": {
+              backgroundColor:
+                chartGoalType === "screentime" &&
+                totalBehaviorValue > totalRecommendedValue * 2
+                  ? "#FF6961"
+                  : chartGoalType === "screentime" &&
+                    totalBehaviorValue === totalRecommendedValue
+                  ? "#8884d8"
+                  : chartGoalType === "screentime" &&
+                    totalBehaviorValue > totalRecommendedValue
+                  ? "#FFC000"
+                  : chartGoalType === "screentime" &&
+                    totalBehaviorValue < totalRecommendedValue
+                  ? "#77DD77"
+                  : totalBehaviorValue > totalRecommendedValue
+                  ? "#77DD77"
+                  : totalBehaviorValue === totalRecommendedValue
+                  ? "#8884d8"
+                  : totalBehaviorValue < totalRecommendedValue / 2
+                  ? "#FF6961"
+                  : "#FFC000",
+            },
+          }}
+        />
+        <div style={{ width: "10%" }}>
+          {+parseFloat(totalBehaviorValue).toFixed(2)} / {totalRecommendedValue}
+        </div>
+      </div>
     </Box>
   );
 };
