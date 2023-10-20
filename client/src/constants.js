55b434dddea8b10c9680d4c2ef578ed8bfb1f15e
Bruce Quach <quachbruce@gmail.com>
2023-10-20 18:48:19 -0500

    Live server

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const DATABASE_URL = "http://localhost:3001";
+export const DATABASE_URL = "https://project-proudme.onrender.com";
 // https://project-proudme.onrender.com
 // http://localhost:3001
