72e9c336d1d7f5f27e218a12f3de2e1f233cfdc2
Hudson Liu <haozeliu@andrew.cmu.edu>
2024-10-25 16:16:17 -0400

    a

--- client/src/screens/journal/phone/activitiy.js
+++ client/src/screens/journal/phone/activitiy.js
@@ -15,7 +15,6 @@ import ClickAwayListener from '@mui/material/ClickAwayListener';
 import Grow from '@mui/material/Grow';
 import Paper from '@mui/material/Paper';
 import Popper from '@mui/material/Popper';
-import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
 import Stack from '@mui/material/Stack';
 
