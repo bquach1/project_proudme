c7d550a1fa4ea4855079e7a4e254a8ff0aa51d21
Hudson Liu <169018073+HudsonLiuHCL@users.noreply.github.com>
2024-12-18 16:06:27 -0500

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
-export const DATABASE_URL = " http://localhost:3001";
+export const DATABASE_URL = " https://project-proudme.onrender.com";
 
-// "https://project-proudme.onrender.com
-//
+// 
+//  http://localhost:3001
 //
 
 export const BEHAVIOR_COLORS = {
